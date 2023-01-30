2d1f26bc967b744e199a03bf523305b598cebb40
Justin Li <your_email@example.com>
2023-01-30 10:37:12 -0800

    added highscore fix

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import { TimerContext } from '../App.js';
 
 function Button(props) {
   let setSize = props.setSize
-  let {isActive, isScrambled, setScrambled, setIsActive, time, setTime} = useContext(TimerContext);
+  let {isActive, isScrambled, setScrambled, setIsActive, time, setTime, isSolved, setIsSolved, bestTime, bestTime2, bestTime3,} = useContext(TimerContext);
   let size = props.size
   
   return (
@@ -13,6 +13,7 @@ function Button(props) {
       setScrambled(false); 
       setIsActive(false);
       setTime(0);
+      setIsSolved(false);
       }} >
         <h2 className="text">{props.size}x{props.size}</h2>
     </div>
--- src/components/Scrambleboard.js
+++ src/components/Scrambleboard.js
@@ -6,38 +6,109 @@ import { fullScramble } from '../hooks/useFullScramble.js';
 
 function Scrambleboard(props) {
     let {edges, setEdges} = useContext(EdgeContext);
-    let {isActive, isScrambled, setScrambled, setIsActive, time, setTime, isSolved, setIsSolved} = useContext(TimerContext);
+    let {isActive, isScrambled, setScrambled, setIsActive, time, setTime, isSolved, setIsSolved, bestTime, bestTime2, bestTime3,} = useContext(TimerContext);
     let size = props.size;
-    let opacitized = { opacity: "80%", 
-        transition: "opacity 0.2s ease-out 100ms"};
-    if (isSolved) {
-        
-    }
-    let textstyle = {
-        display: "block"
-    }
-    if (isScrambled) {
-        opacitized = { opacity: "0%", 
+    let highscore = 0
+    switch (size) {
+        case 2:
+            highscore = bestTime
+            break
+        case 3:
+            highscore = bestTime2
+            break
+        case 4:
+            highscore = bestTime3
+            break
+      }
+      
+
+    let opacitized = { opacity: "0%", 
         width: "0%",
         transition: "opacity 0.2s ease-out 100ms, width 0.3s ease-out 100ms"};
-        textstyle = {
-            display: "none"
-        }
+    let textstyle = {
+        display: "none"
     }
+
+    if (!isScrambled) {
+        if (isSolved) {
+            opacitized = { opacity: "80%", 
+            transition: "opacity 0.2s ease-out 100ms"}
+            textstyle = {
+                display: "flex"
+            }
+            if (time == highscore) {
+                opacitized = { 
+                    opacity: "80%", 
+                    backdropFilter: "blur(30px) invert(70%)",
+                    transition: "opacity 0.2s ease-out 100ms"
+                  };
+                  
+                return (
+                    <div 
+                        style={opacitized} 
+                        className="scramble">
+                        <h4 className="headingtext">{"new high score!"}</h4>
+                        <h2 className="timetext">{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</h2>
+                        <button 
+                        onClick={() => { 
+                            fullScramble(edges, setEdges, props.size); 
+                            setScrambled(true); 
+                            setIsActive(true);
+                            setIsSolved(false);
+                            setTime(0);
+                            }} 
+                        style={textstyle} className="text4">start</button>
+                        
+                    </div>
+                ); 
+            } else {
+                return (
+                    <div 
+                        style={opacitized} 
+                        className="scramble">
+                        <h4 className="headingtext">{"here's your time!"}</h4>
+                        <h2 className="timetext">{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</h2>
+                        <h4 className="headingtext">{"best time"}</h4>
+                        <h2 className="besttimetext">{("0" + Math.floor((highscore / 60000) % 60)).slice(-2)}:{("0" + Math.floor((highscore / 1000) % 60)).slice(-2)}</h2>
+                        
+                        <button 
+                        onClick={() => { 
+                            fullScramble(edges, setEdges, props.size); 
+                            setScrambled(true); 
+                            setIsActive(true);
+                            setIsSolved(false);
+                            setTime(0);
+                            }} 
+                        style={textstyle} className="text4">start</button>
+                        
+                    </div>
+                ); 
+            }  
+        } else {
+            opacitized = { opacity: "80%", 
+            transition: "opacity 0.2s ease-out 100ms"}
+            textstyle = {
+                display: "flex"
+            }
+        } 
+    }
+
+
     return (
         
         <div 
             style={opacitized} 
+            className="scramble">
+            <button 
             onClick={() => { 
-            fullScramble(edges, setEdges, props.size); 
-            setScrambled(true); 
-            setIsActive(true);
-            setIsSolved(false);
-            setTime(0);
-            }} 
-            className="scramble"
-        >
-            <h2 style={textstyle} className="text3">start</h2>
+                fullScramble(edges, setEdges, props.size); 
+                setScrambled(true); 
+                setIsActive(true);
+                setIsSolved(false);
+                setTime(0);
+                }} 
+            style={textstyle} className="text3">start</button>
+            
         </div>
     );      
   }
